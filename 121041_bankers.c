66b31c8e451a636b28ced8d16390d0f475358423
Raj Thakkar <rajt.7795@gmail.com>
2015-03-14 01:05:41 +0530

    lab7 submission.

---
+++ 121041_bankers.c
@@ -0,0 +1,126 @@
+// Banker's algorithm
+
+#include<stdio.h>
+
+void main()
+{
+	int p, r ,i,j;
+
+	
+	printf("\nEnter number of processes: ");
+	scanf("%d", &p);
+	printf("\nEnter number of resources: ");
+	scanf("%d", &r);
+	
+	int ava[r], total[r]; //Total resources
+	int curr[p][r];
+	int max[p][r];
+	int need[p][r];
+	int exe, safe;
+	int running[p];
+	int end=0;
+	
+	for( i=0 ; i<r ; i++)
+	{
+		printf("\nEnter max num of resource %d: ", i);
+		scanf("%d", &total[i]);
+		ava[i]=total[i];
+		}
+		
+	for( i=0 ; i<p ; i++ )
+	{
+		for( j=0 ; j<r ; j++ )
+		{
+		printf("\nCurrent[%d][%d]= ", i, j);
+		scanf("%d", &curr[i][j]);
+		}
+	}
+	
+	for( i=0 ; i<p ; i++ )
+	{
+		for( j=0 ; j<r ; j++ )
+		{
+		printf("\nMax Claim[%d][%d]= ", i, j);
+		scanf("%d", &max[i][j]);
+		}
+	}
+
+// Calculate Need	
+	for( i=0 ; i<p ; i++ )
+	{
+		for( j=0 ; j<r ; j++ )
+		{
+		need[i][j]= max[i][j] - curr[i][j];
+		printf("%d ", need[i][j]);
+		}
+		printf("\n");
+	}
+	
+	
+	
+
+// Calculate available
+	//printf("\nAvailable matrix: ");
+	for( j=0 ; j<r ; j++)
+	{
+	for( i=0 ; i<p ; i++)
+	{
+		ava[j]= ava[j]- curr[i][j];
+		//printf("\n%d = %d", j, ava[j]);
+	}
+	}
+	
+	
+	
+/*	for( i=0  ; i<p ; i++)
+//		running[i]=0;	
+// LOGIC
+	//printf("\nbefore logic");
+		
+	while( end<p )	// when all processes are executed/ deadlock detected
+	{
+	safe=0;*/
+	
+	for( i=0; i< p ; i++)
+	{
+		safe=0;
+			exe=1;
+				for(j=0 ; j< r ; j++)
+				{
+					if(need[i][j] > ava[j])
+					{
+					exe=0;
+					break;
+					}
+				}
+			
+				if(exe==1)
+				{
+					for(j=0 ; j<r ; j++){
+					ava[j]= ava[j] + need[i][j];}
+					safe=1;
+					end++;
+					running[i]=end;
+		
+				
+				}
+	}
+
+	if(safe)
+	{
+		printf("\nSafe state. No deadlocks. ");
+	}
+	else
+	printf("\nUnsafe state. potential deadlock.");	
+}
+	
+	
+	
+		
+			
+	
+
+	
+	
+	
+	
